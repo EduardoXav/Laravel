2d5673a0f8d283262491432d0af60419df78d1fa
Eduardo Da Silva <93222151+EduardoXavier612@users.noreply.github.com>
2023-03-03 12:33:53 +0100

    fin du TP1

---
+++ database/migrations/2023_03_03_103422_create_table_reservation_vehicule.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableReservationVehicule extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+            //
+        Schema::create('table_reservation_vehicule', function(bluePrint $table){
+            $table -> String('reservation_id')
+                   -> foreign('reservation_id')
+                   -> references('codeReservation')
+                   -> on('reservation')
+                   -> onDelete('cascade');
+            $table -> String('vehicule_id')
+                   -> foreign('vehicule_id')
+                   -> references('matricule')
+                   -> on('reservation');
+            $table -> primary(['vehicule_id', 'reservation_id']);
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
